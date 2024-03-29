4f79125be20267310552a0b9497c5cd9a8decda8
hossam baiz <baizmohamedhossam@gmail.com>
2024-03-29 17:11:16 +0000

    This commit adjusts the URL configuration to set the login page as the root URL for the project. Changes are made to the urls.py files in both the blog and users apps to ensure that the login page is the default landing page when accessing the website.

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home-page'),
+    path('blog/', views.index, name='home-page'),
     path('about/', views.about, name='about-page'),
 ]
