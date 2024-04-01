dfc812fff5211bf07036947a2ee026994b89de6f
hossam baiz <baizmohamedhossam@gmail.com>
2024-04-01 14:05:52 +0000

    This commit adds a new path in the blog application to handle post details. It includes modifications to views, URLs, and templates to enable users to view details of individual posts.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('blog/', views.index, name='home-page'),
     path('about/', views.about, name='about-page'),
+    path('post_details/<int:pk>/', views.post_detail, name='details-page'),
 ]
